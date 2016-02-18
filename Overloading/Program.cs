baedf4a1abb6e93d23eac291f874696ac20327e6
jkarthick89 <jkarthick89@gmail.com>
2016-02-19 01:56:56 +0530

    First commit

---
+++ Overloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = GetValue(2);
+        }
+
+        static int GetValue(string i)
+        {
+            return Convert.ToInt32(i);
+        }
+
+        static string GetValue(int i)
+        {
+            return i.ToString();
+        }
+    }
+}
